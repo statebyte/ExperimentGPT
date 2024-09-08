b52aa986176189e89e4b17d71ff9bd9073914d5b
FIVE <elsaudomar@gmail.com>
2024-09-08 13:23:53 +0300

    Up

---
+++ README.md
@@ -0,0 +1,3 @@
+# ExperimentGPT
+
+I am testing different models for development and automation.
--- generate_gpt.py
+++ generate_gpt.py
@@ -2,7 +2,7 @@ import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # Загрузка модели и токенизатора GPT-3
-model_name = 'ai-forever/rugpt3large_based_on_gpt2'
+model_name = 'ai-forever/ruGPT-3.5-13B'
 cache_dir = "D:/models/huggingface"
 model = GPT2LMHeadModel.from_pretrained(model_name, cache_dir=cache_dir)
 tokenizer = GPT2Tokenizer.from_pretrained(model_name, cache_dir=cache_dir)
@@ -23,7 +23,7 @@ def generate_text(prompt, max_length=100, temperature=1.0, top_k=50):
             # max_length=max_length, 
             temperature=temperature,
             max_new_tokens=max_length,
-            # num_beams=4, 
+            num_beams=4, 
             top_k=top_k,
             do_sample=True
         )
